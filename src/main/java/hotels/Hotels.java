b380075c3b3614bc93e7131bb52aaa64f99f5021
Mason-King <73730655+Mason-King@users.noreply.github.com>
2021-03-30 09:38:05 -0500

    Initial Commit

---
+++ src/main/java/hotels/Hotels.java
@@ -0,0 +1,17 @@
+package hotels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hotels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
